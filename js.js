3c02b71145aafdaa99e9525b225f6aabef5d900d
AkhilPrakash <nathantheresa22@gmail.com>
2025-05-07 11:10:19 +0530

    Functions

--- js.js
+++ js.js
@@ -5,3 +5,16 @@ var x =10;
 function getname(){
     console.log("Namaste javascript");
 }
+//functions
+var s=10;
+a();
+b();
+console.log(s);
+function a(){
+    var s=100;
+    console.log(s)
+};
+function b(){
+    var s=200;
+    console.log(s);
+};
