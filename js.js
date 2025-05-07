15e81f0c9cec15f1900e472539c3fbb7389e53e1
AkhilPrakash <nathantheresa22@gmail.com>
2025-05-07 18:41:20 +0530

    block-scope

--- js.js
+++ js.js
@@ -1,6 +1,11 @@
-console.log(b);
 let a=10;
-console.log(a);
-
-
-var b=100;
+{
+    let a=100;
+    var b=20;
+    const c=45;
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
+console.log(a)
+//let is block scope and outside curly braces let and inside the curly braces let are different
