1594bc4083f1d00ca49794106f446069f3cc4936
AkhilPrakash <nathantheresa22@gmail.com>
2025-05-07 18:44:21 +0530

    Global-scope

--- js.js
+++ js.js
@@ -1,11 +1,11 @@
-let a=10;
+var a=10;
 {
-    let a=100;
-    var b=20;
-    const c=45;
+    var a=20;
+    let b=30;
+    const c=40;
     console.log(a)
     console.log(b)
     console.log(c)
-}
-console.log(a)
-//let is block scope and outside curly braces let and inside the curly braces let are different
+};
+console.log(a);
+// it is the global scope so var is a global scope 
