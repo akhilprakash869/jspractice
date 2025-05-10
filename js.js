1fe7be1af80ddfdf64f7bf1404a822af3226df2a
AkhilPrakash <nathantheresa22@gmail.com>
2025-05-11 00:05:04 +0530

    array destruture

--- js.js
+++ js.js
@@ -1,25 +1,16 @@
-function a(){
-    var x=30;
-    function b(){
-        var y=40;
-        function c(){
-            console.log(x,y);
-
-        }
-       c();
-    }
-   b();
-}
-a();
-// encapsulation
-class  Person{
+//encapsulation
+class person{
     constructor(name,age){
         this.name=name;
         this.age=age;
     }
     greet(){
-        console.log(`hello ,my name is ${this.name}`);
+        console.log(`my name is ${this.name}`);
     }
 }
-const person1=new Person("Akhil",25);
-person1.greet();
+const person1=new person("akhoi",25);
+person1.greet(); 
+//array
+const array=[1,2,3,4,5,6,6,1,2,3];
+const [a,b,c,...d]=array;
+console.log(a);
