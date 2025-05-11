aedcdeb91ccc1419d79385c332b6e21f0fb29acb
AkhilPrakash <nathantheresa22@gmail.com>
2025-05-11 15:40:57 +0530

    hoisting

--- js.js
+++ js.js
@@ -1,19 +1,10 @@
-//encapsulation
-class person{
-    constructor(name,age){
-        this.name=name;
-        this.age=age;
-    }
-    greet(){
-        console.log(`my name is ${this.name}`);
-    }
-}
-const person1=new person("akhoi",25);
-person1.greet(); 
-//array
-const array=[1,2,3,4,5,6,6,1,2,3];
-const [a,b,c,...d]=array;
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
+console.log(x)
+add(5,8);
+
+var x=10;
+function add(a,b){
+  var sum =a+b;
+  console.log(sum)
+};
+
+//Hoisting
